df3a85251fa8ad6d8713da7b83aab754e3d7705f
Alsu <alsu20082008@yandex.ru>
2015-03-18 10:28:51 +0300

    в комментариях к 1 фазе попыталась сделать таблицу А

--- GCD_Algorithm/src/main/Ph1_Precomputation.java
+++ GCD_Algorithm/src/main/Ph1_Precomputation.java
@@ -20,23 +20,77 @@ public class Ph1_Precomputation {
 		
 		A = new BigInteger[k];
 		A[0] = null;
-		A[1] = BigInteger.ONE;
-		A[2] = BigInteger.ONE;
+		A[1] = new BigInteger("1");
+		A[2] = new BigInteger("1");
 		A[3] = new BigInteger("2");
 		A[4] = new BigInteger("2");
-		A[5] = BigInteger.ONE;
-		A[6] = BigInteger.ONE;
+		A[5] = new BigInteger("1");
+		A[6] = new BigInteger("1");
 		
 		/*
-		for (int a = 1; a < Math.sqrt(k)+1; a++){
-			for(int b = 1; b < Math.sqrt(k)+1; b++){
-				
-			}
-		}
+		
+		A = new BigInteger[k];
+		A[0] = null;
+		A[1] = new BigInteger("3");
+		A[2] = new BigInteger("2");
+		A[3] = new BigInteger("4");
+		A[4] = new BigInteger("3");
+		A[5] = new BigInteger("2");
+		A[6] = new BigInteger("3");
+		
+		A = new BigInteger[k];
+		A[0] = null;
+		A[1] = new BigInteger("3");
+		A[2] = new BigInteger("2");
+		A[3] = new BigInteger("2");
+		A[4] = new BigInteger("1");
+		A[5] = new BigInteger("1");
+		A[6] = new BigInteger("1");
+		
+		A = new BigInteger[k];
+		A[0] = null;
+		A[1] = new BigInteger("1");
+		A[2] = new BigInteger("1");
+		A[3] = new BigInteger("1");
+		A[4] = new BigInteger("1");
+		A[5] = new BigInteger("1");
+		A[6] = new BigInteger("1");
+		
+		A = new BigInteger[k];
+		A[0] = null;
+		A[1] = new BigInteger("-1");
+		A[2] = new BigInteger("-1");
+		A[3] = new BigInteger("-2");
+		A[4] = new BigInteger("-2");
+		A[5] = new BigInteger("-1");
+		A[6] = new BigInteger("-1");
 		*/
-		/************************/
 		
 		int k1 = (int)Math.sqrt(k)+1;
+		/*
+		BigInteger[][] X = new BigInteger[2*k1][2*k1];
+		
+		BigInteger a_from = new BigInteger(""+ (int)-(Math.sqrt(k)+1));
+		BigInteger a_to = new BigInteger(""+ (int) Math.sqrt(k)+1);
+		
+		BigInteger b_from = new BigInteger(""+ (int)-(Math.sqrt(k)+1));
+		BigInteger b_to = new BigInteger(""+ (int) Math.sqrt(k)+1);
+		
+		System.out.println("X = ");
+		
+		for(BigInteger a = a_from; a.compareTo(a_to) == -1; a.add(BigInteger.ONE)){
+			for(BigInteger b = b_from; b.compareTo(b_to) == -1; b.add(BigInteger.ONE)){
+				X[a.intValue()][b.intValue()] = ((b.multiply(a.modInverse(new BigInteger(""+k)))).mod(new BigInteger(""+k))).negate();
+				System.out.print(X[a.intValue()][b.intValue()]);
+			}
+			System.out.println();
+		}
+		
+		*/
+		
+		/************************/
+		
+		
 		Eratosthenes.fillSieve(k+1);
 		
 		for(int x = 0; x < k; x++){
